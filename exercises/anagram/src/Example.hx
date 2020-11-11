3767702195bccbac2992635743bdd0ce061adf9c
Mohammad Foda <23182653+mfoda@users.noreply.github.com>
2020-11-11 23:46:54 +0800

    Minor naming, formatting, style adjustments

--- exercises/anagram/src/Example.hx
+++ exercises/anagram/src/Example.hx
@@ -4,14 +4,14 @@ class Anagram {
     public static function findAnagrams(subject: String, candidates: Array<String>): Array<String> {
         var anagrams = [for (val in candidates) 
                         if (subject.toLowerCase() != val.toLowerCase())
-                        if (sort_string(subject.toLowerCase()) == sort_string(val.toLowerCase()))
+                        if (sortString(subject.toLowerCase()) == sortString(val.toLowerCase()))
                         val];
         return anagrams;
     }
     
-    private static function sort_string(string:String): String {
-        var _string = string.split("");
-        _string.sort((s1, s2) -> s1 < s2 ? 1 : -1);
-        return _string.join("");
+    private static function sortString(string: String): String {
+        var chars = string.split("");
+        chars.sort((c1, c2) -> c1 < c2 ? 1 : -1);
+        return chars.join("");
     }
 }
