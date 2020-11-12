a6445b55b8ec981cff69248d3129bb948d38f7e0
Mohammad Foda <23182653+mfoda@users.noreply.github.com>
2020-11-12 11:47:36 +0800

    Add two-fer exercise

---
+++ exercises/two-fer/src/Example.hx
@@ -0,0 +1,7 @@
+package;
+
+class TwoFer {
+	public static function twoFer(?name = "you"):String {
+		return 'One for $name, one for me.';
+	}
+}
---
+++ exercises/two-fer/src/TwoFer.hx
@@ -0,0 +1,7 @@
+package;
+
+class TwoFer {
+    public static function twoFer(name: String): String {
+        throw "Not Implemented"; // Delete this statement and write your own implementation.
+    }
+}
