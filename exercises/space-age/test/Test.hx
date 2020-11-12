e13f382faccbcba622b6e5a2f5666e457996d91f
Mohammad Foda <23182653+mfoda@users.noreply.github.com>
2020-11-13 05:07:21 +0800

    Skip all but first test case

--- exercises/space-age/test/Test.hx
+++ exercises/space-age/test/Test.hx
@@ -10,24 +10,31 @@ class Test extends buddy.SingleSuite {
 				new SpaceAge(1000000000).onEarth().should().be(31.69);
 			});
 			it("age on Mercury", {
+				pending("Skipping");
 				new SpaceAge(2134835688).onMercury().should().be(280.88);
 			});
 			it("age on Venus", {
+				pending("Skipping");
 				new SpaceAge(189839836).onVenus().should().be(9.78);
 			});
 			it("age on Mars", {
+				pending("Skipping");
 				new SpaceAge(2129871239).onMars().should().be(35.88);
 			});
 			it("age on Jupiter", {
+				pending("Skipping");
 				new SpaceAge(901876382).onJupiter().should().be(2.41);
 			});
 			it("age on Saturn", {
+				pending("Skipping");
 				new SpaceAge(2000000000).onSaturn().should().be(2.15);
 			});
 			it("age on Uranus", {
+				pending("Skipping");
 				new SpaceAge(1210123456).onUranus().should().be(0.46);
 			});
 			it("age on Neptune", {
+				pending("Skipping");
 				new SpaceAge(1821023456).onNeptune().should().be(0.35);
 			});
 		});
