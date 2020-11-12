a4bc056c9b6e37719f5fdcdcbaed63efd96dbd11
Mohammad Foda <23182653+mfoda@users.noreply.github.com>
2020-11-12 11:50:19 +0800

    Fix formatting

--- exercises/two-fer/src/Example.hx
+++ exercises/two-fer/src/Example.hx
@@ -1,7 +1,7 @@
 package;
 
 class TwoFer {
-	public static function twoFer(?name = "you"):String {
+	public static function twoFer(?name = "you"): String {
 		return 'One for $name, one for me.';
 	}
 }
