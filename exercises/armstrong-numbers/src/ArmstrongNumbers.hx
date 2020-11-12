6d4bb1b7df33bc3e2c38f56714f14ce00b7298c4
Mohammad Foda <23182653+mfoda@users.noreply.github.com>
2020-11-12 10:03:29 +0800

    Add armstrong-numbers exercise

---
+++ exercises/armstrong-numbers/src/ArmstrongNumbers.hx
@@ -0,0 +1,7 @@
+package;
+
+class ArmstrongNumbers {
+    public static function isArmstrongNumber(number: Int): Bool {
+        throw "Not Implemented"; // Delete this statement and write your own implementation.
+    }
+}
