77f1f7c0c093d84903a831fa445741ff60ede149
Mohammad Foda <23182653+mfoda@users.noreply.github.com>
2020-11-14 03:26:45 +0800

    Skip all but first test case

--- exercises/robot-name/test/Test.hx
+++ exercises/robot-name/test/Test.hx
@@ -11,22 +11,27 @@ class Test extends buddy.SingleSuite {
 				robot.name.should.match(~/^[A-Z]{2}\d{3}$/);
 			});
 			it("name is the same each time", {
+				pending("Skipping");
 				robot.name.should.be(robot.name);
 			});
 			it("different robots have different names", {
+				pending("Skipping");
 				var robot2 = new Robot();
 				robot2.name.should.not.be(robot.name);
 			});
 			it("can reset robot name", {
+				pending("Skipping");
 				var originalName = robot.name;
 				robot.reset();
 				robot.name.should.not.be(originalName);
 			});
 			it("after reset the name is valid", {
+				pending("Skipping");
 				robot.reset();
 				robot.name.should.match(~/^[A-Z]{2}\d{3}$/);
 			});
 			it("robot names are unique", {
+				pending("Skipping");
 				var names = new Map<String, Bool>();
 				for (i in 0...10000) {
 					var robot = new Robot();
