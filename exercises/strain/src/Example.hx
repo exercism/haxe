f829072362516b25b76adb7285f84c198204caa4
Mohammad Foda <23182653+mfoda@users.noreply.github.com>
2020-11-20 11:55:03 +0800

    Fix formatting

--- exercises/strain/src/Example.hx
+++ exercises/strain/src/Example.hx
@@ -4,16 +4,16 @@ class ArrayExt {
 	public static function keep<T>(array: Array<T>, fun: T -> Bool) {
 		return [
 			for (item in array)
-                if (fun(item)) 
-                    item
+				if (fun(item)) 
+					item
 		];
 	}
 
 	public static function discard<T>(array: Array<T>, fun: T -> Bool) {
 		return [
 			for (item in array)
-                if (!fun(item)) 
-                    item
+				if (!fun(item)) 
+					item
 		];
 	}
 }
