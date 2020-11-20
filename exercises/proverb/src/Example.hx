79aeded006a27056585cff0afb03c0deb8a6a7d4
Mohammad Foda <23182653+mfoda@users.noreply.github.com>
2020-11-20 11:57:18 +0800

    Fix formatting

--- exercises/proverb/src/Example.hx
+++ exercises/proverb/src/Example.hx
@@ -4,16 +4,16 @@ typedef Item  = String;
 typedef Verse = String;
 
 class Proverb {
-    public static function recite(strings: Array<Item>): Array<Verse> {
-        if (strings.length == 0)
+	public static function recite(strings: Array<Item>): Array<Verse> {
+		if (strings.length == 0)
 			return [];
 
 		var verses = [
 			for (i in 0...strings.length - 1)
 				'For want of a ${strings[i]} the ${strings[i + 1]} was lost.'
-        ];
-        verses.push('And all for the want of a ${strings[0]}.');
+		];
+		verses.push('And all for the want of a ${strings[0]}.');
 
 		return verses;
-    } 
+	}
 }
