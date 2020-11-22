08e383738e33c7b6ef107b2a23c4ad34dbf7a5bd
Mohammad Foda <23182653+mfoda@users.noreply.github.com>
2020-11-22 16:23:19 +0800

    Replace functions with variables

--- exercises/yacht/src/Example.hx
+++ exercises/yacht/src/Example.hx
@@ -4,20 +4,21 @@ using Lambda;
 
 class Yacht {
 	public static function score(dice: Array<Int>, category: String): Int {
-		function sum()
-			return dice.fold((x, acc) -> x + acc, 0);
 		function count(n)
 			return dice.count(x -> x == n);
-		function yacht()
-			return dice.exists(x -> x != dice[0]) ? 0 : 50;
-		function house()
-			return dice.exists(x -> count(x) == 2) && dice.exists(x -> count(x) == 3) ? sum() : 0;
-		function fourOfKind()
-			return 4 * switch (dice.find(x -> count(x) >= 4)) { case null: 0; case n: n; };
-		function lstraight()
-			return [1, 2, 3, 4, 5].foreach(dice.contains) ? 30 : 0;
-		function bstraight()
-			return [2, 3, 4, 5, 6].foreach(dice.contains) ? 30 : 0;
+
+		var sum  				= // sum of the dice
+			dice.fold((x, acc) -> x + acc, 0);
+		var yachtScore  		= // 50 if all dices showing the same face
+			dice.exists(x -> x != dice[0]) ? 0 : 50;
+		var fullHouseScore  	= // total of the dice
+			dice.exists(x -> count(x) == 2) && dice.exists(x -> count(x) == 3) ? sum : 0;
+		var fourOfKindScore  	= // total of the four dice
+			4 * switch (dice.find(x -> count(x) >= 4)) { case null: 0; case n: n; };
+		var lStraightScore  	= // 30 points
+			[1, 2, 3, 4, 5].foreach(dice.contains) ? 30 : 0;
+		var bStraightScore  	= // 30 points
+			[2, 3, 4, 5, 6].foreach(dice.contains) ? 30 : 0;
 
 		return switch (category) {
 			case "ones":            1 * count(1);
@@ -26,12 +27,12 @@ class Yacht {
 			case "fours":           4 * count(4);
 			case "fives":           5 * count(5);
 			case "sixes":           6 * count(6);
-			case "yacht":           yacht();
-			case "full house":      house();
-			case "four of a kind":  fourOfKind();
-			case "little straight": lstraight();
-			case "big straight":    bstraight();
-			case "choice":          sum();
+			case "yacht":           yachtScore;
+			case "full house":      fullHouseScore;
+			case "four of a kind":  fourOfKindScore;
+			case "little straight": lStraightScore;
+			case "big straight":    bStraightScore;
+			case "choice":          sum;
 			case _:                 throw "invalid category";
 		}
 	}
