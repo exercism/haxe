7728d63a65b90e3fb93eee3a103eaf1ededc94da
Mohammad Foda <23182653+mfoda@users.noreply.github.com>
2020-11-25 00:04:26 +0800

    Format Example.hx

--- exercises/forth/src/Example.hx
+++ exercises/forth/src/Example.hx
@@ -17,138 +17,139 @@ typedef Instruction = String;
 
 class Forth {
 	public static function evaluate(instructions: Array<Instruction>): Stack {
-        // check if instruction is a Define
-        function isDefine(ins) return ~/^:.*;$/.match(ins);
+		// check if instruction is a Define
+		function isDefine(ins)
+			return ~/^:.*;$/.match(ins);
 
-        var stack = [];
-        var defines = [];
+		var stack = [];
+		var defines = [];
 		for (instruction in instructions) {
-            // instructions are case-insensitive
-            instruction = instruction.toLowerCase();
+			// instructions are case-insensitive
+			instruction = instruction.toLowerCase();
 
-            // stack defines and apply recursively
-            if (isDefine(instruction)) {
-                defines.push(makeDefine(instruction));
-                continue;
-            }
-            instruction = transform(instruction, defines);
+			// stack defines and apply recursively
+			if (isDefine(instruction)) {
+				defines.push(makeDefine(instruction));
+				continue;
+			}
+			instruction = transform(instruction, defines);
 
 			for (token in instruction.split(" ")) {
-                token = transform(token, defines);
+				token = transform(token, defines);
 				switch (token) {
-                    case "+": 
-                        add(stack);
-                    case "-": 
-                        subtract(stack);
-                    case "*": 
-                        multiply(stack);
-                    case "/": 
-                        divide(stack);
-                    case "dup": 
-                        dup(stack);
-                    case "drop": 
-                        drop(stack);
-                    case "swap": 
-                        swap(stack);
-                    case "over": 
-                        over(stack);
+					case "+":
+						add(stack);
+					case "-":
+						subtract(stack);
+					case "*":
+						multiply(stack);
+					case "/":
+						divide(stack);
+					case "dup":
+						dup(stack);
+					case "drop":
+						drop(stack);
+					case "swap":
+						swap(stack);
+					case "over":
+						over(stack);
 					case n if (Std.parseInt(n) != null):
-                        stack.push(Std.parseInt(n));
-                    case _:
-                        throw StackError.UndefinedOperation;
+						stack.push(Std.parseInt(n));
+					case _:
+						throw StackError.UndefinedOperation;
 				}
 			}
-        }
+		}
 		return stack;
-    }
+	}
 
-    private static function transform(instruction: Instruction, defines: Array<Define>): Instruction {
-        if (defines.empty())
-            return instruction;
+	private static function transform(instruction: Instruction, defines: Array<Define>): Instruction {
+		if (defines.empty())
+			return instruction;
 
-        return transform(defines.pop()(instruction), defines);
-    }
+		return transform(defines.pop()(instruction), defines);
+	}
 
-    private static function makeDefine(instruction: String): Define {
-        var pattern = ~/^: (\S+) (.+) ;$/;
-        pattern.match(instruction);
-        var word = pattern.matched(1);
-        var replacement = pattern.matched(2);
-        if (~/[[1-9]/.match(word))
-            throw StackError.IllegalOperation;
+	private static function makeDefine(instruction: String): Define {
+		var pattern = ~/^: (\S+) (.+) ;$/;
+		pattern.match(instruction);
+		var word = pattern.matched(1);
+		var replacement = pattern.matched(2);
+		if (~/[[1-9]/.match(word))
+			throw StackError.IllegalOperation;
 
-        return (ins: String) -> return ins.replace(word, replacement);
-    }
+		return (ins:String) -> return ins.replace(word, replacement);
+	}
 
-    private static function validateBinaryOp(a: Int, b: Int): Void {
-        if (a == null && b == null)
-            throw StackError.EmptyStack;
-        if (a == null || b == null)
-            throw StackError.OneValue;
-    }
+	private static function validateBinaryOp(a: Int, b: Int): Void {
+		if (a == null && b == null)
+			throw StackError.EmptyStack;
+		if (a == null || b == null)
+			throw StackError.OneValue;
+	}
 
-    private static function add(stack: Stack): Void {
-        var b = stack.pop(), a = stack.pop();
-        validateBinaryOp(a, b);
+	private static function add(stack: Stack): Void {
+		var b = stack.pop(), a = stack.pop();
+		validateBinaryOp(a, b);
 
-        stack.push(a + b);
-    }
+		stack.push(a + b);
+	}
 
-    private static function subtract(stack: Stack): Void {
-        var b = stack.pop(), a = stack.pop();
-        validateBinaryOp(a, b);
+	private static function subtract(stack: Stack): Void {
+		var b = stack.pop(), a = stack.pop();
+		validateBinaryOp(a, b);
 
-        stack.push(a - b);
-    }
+		stack.push(a - b);
+	}
 
-    private static function multiply(stack: Stack): Void {
-        var b = stack.pop(), a = stack.pop();
-        validateBinaryOp(a, b);
+	private static function multiply(stack: Stack): Void {
+		var b = stack.pop(), a = stack.pop();
+		validateBinaryOp(a, b);
 
-        stack.push(a * b);
-    }
+		stack.push(a * b);
+	}
 
-    private static function divide(stack: Stack): Void {
-        var b = stack.pop(), a = stack.pop();
-        validateBinaryOp(a, b);
+	private static function divide(stack: Stack): Void {
+		var b = stack.pop(), a = stack.pop();
+		validateBinaryOp(a, b);
 
-        if (b == 0)
-            throw StackError.DivideByZero;
+		if (b == 0)
+			throw StackError.DivideByZero;
 
-        stack.push(Std.int(a / b));
-    }
+		stack.push(Std.int(a / b));
+	}
 
-    private static function dup(stack: Stack): Void {
-        var b = stack.pop();
+	private static function dup(stack: Stack): Void {
+		var b = stack.pop();
 
-        if (b == null)
-            throw StackError.EmptyStack;
+		if (b == null)
+			throw StackError.EmptyStack;
 
-        stack.push(b);
-        stack.push(b);
-    }
+		stack.push(b);
+		stack.push(b);
+	}
 
-    private static function drop(stack: Stack): Void {
-        var b = stack.pop();
+	private static function drop(stack: Stack): Void {
+		var b = stack.pop();
 
-        if (b == null)
-            throw StackError.EmptyStack;
-    }
+		if (b == null)
+			throw StackError.EmptyStack;
+	}
 
-    private static function swap(stack: Stack): Void {
-        var b = stack.pop(), a = stack.pop();
-        validateBinaryOp(a, b);
+	private static function swap(stack: Stack): Void {
+		var b = stack.pop(), a = stack.pop();
+		validateBinaryOp(a, b);
 
-        stack.push(b);
-        stack.push(a);
-    }
+		stack.push(b);
+		stack.push(a);
+	}
 
-    private static function over(stack: Stack): Void {
-        var b = stack.pop(), a = stack.pop();
-        validateBinaryOp(a, b);
+	private static function over(stack: Stack): Void {
+		var b = stack.pop(), a = stack.pop();
+		validateBinaryOp(a, b);
 
-        stack.push(a);
-        stack.push(b);
-        stack.push(a);
-    }
+		stack.push(a);
+		stack.push(b);
+		stack.push(a);
+	}
 }
