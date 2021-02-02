bea57bbf59d93a335f569c2562fe6b1dcc738cfc
mfoda <23182653+mfoda@users.noreply.github.com>
2021-02-02 09:35:34 +0800

    Make solution more explicit, fix formatting

--- exercises/luhn/src/Example.hx
+++ exercises/luhn/src/Example.hx
@@ -4,30 +4,37 @@ using StringTools;
 using Lambda;
 
 class Luhn {
-    public static function valid(input: String): Bool {
-        // spaces are allowed but stripped
-        input = input.replace(" ", "");
+	public static function valid(input:String):Bool {
+		// spaces are allowed but stripped
+		input = input.replace(" ", "");
 
-        // single digit is not valid
-        if (input.length <= 1)
-            return false;
+		// single digit is not valid
+		if (input.length <= 1)
+			return false;
 
-        // only digits are allowed
-        if (~/[^0-9]/.match(input))
-            return false;
+		// only digits are allowed
+		if (~/[^0-9]/.match(input))
+			return false;
 
-        // double every 2nd digit, subtracting 9 if result > 9
-        var digits = input.split("").map(Std.parseInt);
-        for (i in 0...Std.int(digits.length / 2)) {
-            var j = digits.length - (2 * i) - 2;
-            digits[j] = (digits[j] * 2);
-            if (digits[j] > 9)
-                digits[j] -= 9;
-        }
+		var sum = 0;
+		var second = false; // tracks every other digit
 
-        function sum(a, b) return a + b;
-        var digitSum = digits.fold(sum, 0);
-        
-        return digitSum % 10 == 0;
-    } 
+		for (i in 0...input.length) {
+			// loop in reverse order
+			var j = input.length - i - 1;
+
+			// convert char code to digit value
+			var temp = input.charCodeAt(j) - '0'.code;
+
+			// double every other number
+			if (second)
+				temp *= 2;
+
+			// subtract 9 if > 9
+			sum += temp > 9 ? temp - 9 : temp;
+			second = !second;
+		}
+
+		return sum % 10 == 0;
+	}
 }
