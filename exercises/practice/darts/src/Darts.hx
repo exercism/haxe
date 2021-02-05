1ee802a84e2a63d6d269dc6b516a8500b665cbac
Mohammad Foda <23182653+mfoda@users.noreply.github.com>
2021-02-06 05:11:40 +0800

    Add darts exercise (#116)
    * Add darts exercise
    
    * Updates darts to v3

---
+++ exercises/practice/darts/src/Darts.hx
@@ -0,0 +1,11 @@
+package;
+
+typedef Point = {
+    x: Float, y: Float
+}
+
+class Darts {
+    public static function score(point: Point): Int {
+        throw "Not Implemented"; // Delete this statement and write your own implementation.
+    }
+}
