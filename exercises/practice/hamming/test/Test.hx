f38b206d2b91810cc519c111be74ad89b3cd8d1e
Mohammad Foda <23182653+mfoda@users.noreply.github.com>
2021-02-12 08:37:40 +0800

    Fix typo in Hamming tests

--- exercises/practice/hamming/test/Test.hx
+++ exercises/practice/hamming/test/Test.hx
@@ -9,23 +9,23 @@ class Test extends buddy.SingleSuite {
 				Hamming.distance("", "").should().be(0);
 			});
 			it("single letter identical strands", {
-				pending("Skpping");
+				pending("Skipping");
 				Hamming.distance("A", "A").should().be(0);
 			});
 			it("single letter different strands", {
-				pending("Skpping");
+				pending("Skipping");
 				Hamming.distance("G", "T").should().be(1);
 			});
 			it("long identical strands", {
-				pending("Skpping");
+				pending("Skipping");
 				Hamming.distance("GGACTGAAATCTG", "GGACTGAAATCTG").should.be(0);
 			});
 			it("long different strands", {
-				pending("Skpping");
+				pending("Skipping");
 				Hamming.distance("GGACGGATTCTG", "AGGACGGATTCT").should.be(9);
 			});
 			it("disallow first strand longer", {
-				pending("Skpping");
+				pending("Skipping");
 				Hamming.distance.bind("AATG", "AAA").should().throwType(haxe.Exception);
 			});
 		});
