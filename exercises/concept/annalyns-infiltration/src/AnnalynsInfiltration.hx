95e29bdb079f0ce79f20c746609a86cb19285e95
mfoda <23182653+mfoda@users.noreply.github.com>
2021-02-26 01:51:35 +0800

    Adapt instructions.md for haxe

--- exercises/concept/annalyns-infiltration/src/AnnalynsInfiltration.hx
+++ exercises/concept/annalyns-infiltration/src/AnnalynsInfiltration.hx
@@ -1,15 +1,15 @@
 function canFastAttack(knightIsAwake:Bool):Bool {
-	throw "Please implement the canFastAttack function";
+	throw "Please implement the canFastAttack() function";
 }
 
 function canSpy(knightIsAwake:Bool, archerIsAwake:Bool, prisonerIsAwake:Bool):Bool {
-	throw "Please implement the canSpy function";
+	throw "Please implement the canSpy() function";
 }
 
 function canSignalPrisoner(archerIsAwake:Bool, prisonerIsAwake:Bool):Bool {
-	throw "Please implement the canSignalPrisoner function";
+	throw "Please implement the canSignalPrisoner() function";
 }
 
 function canFreePrisoner(knightIsAwake:Bool, archerIsAwake:Bool, prisonerIsAwake:Bool, petDogIsPresent:Bool):Bool {
-	throw "Please implement the canFreePrisoner function";
+	throw "Please implement the canFreePrisoner() function";
 }
