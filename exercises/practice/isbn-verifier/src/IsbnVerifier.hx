3bae62520012ae7462891f07264009d0ac7edf10
mfoda <23182653+mfoda@users.noreply.github.com>
2021-02-26 13:13:36 +0800

    Update stub to use module-level fields as Example

--- exercises/practice/isbn-verifier/src/IsbnVerifier.hx
+++ exercises/practice/isbn-verifier/src/IsbnVerifier.hx
@@ -1,7 +1,3 @@
-package;
-
-class IsbnVerifier {
-	public static function isValid(isbn:String):Bool {
-		throw "Not Implemented"; // Delete this statement and write your own implementation.
-	}
+function isValid(isbn:String):Bool {
+	throw "Not Implemented"; // Delete this statement and write your own implementation.
 }
