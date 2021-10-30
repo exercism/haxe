4e68233ae6ea6ecf7fcc9f6b7cc4a1206e3a9fdf
Midyan Hamdoun <midyan.hamdoun@gmail.com>
2021-10-30 14:10:45 +0300

    Add fucntion docs

--- exercises/concept/lucians-luscious-lasagna/src/Lasagna.hx
+++ exercises/concept/lucians-luscious-lasagna/src/Lasagna.hx
@@ -1,15 +1,43 @@
+/**
+ * Outputs the amount of minutes the lasagna should be in the oven for.
+ * 
+ * @return Int
+ */
 function expectedMinutesInOven():Int {
   throw throw "Please implement the expectedMinutesInOven() function";
 }
 
+/**
+ * Determines the amount of minutes the lasagna still needs to remain
+ * in the oven.
+ * 
+ * @param minutesInOven 
+ * @return Int
+ */
 function remainingMinutesInOven(minutesInOven:Int):Int {
   throw "Please implement the remainingMinutesInOven() function";
 }
 
+/**
+ * Determines the amount of minutes it takes to put the given
+ * number of layers when each layer takes 2 minutes.
+ * 
+ * @param layers 
+ * @return Int
+ */
 function preparationTimeInMinutes(layers:Int):Int {
   throw "Please implement the preparationTimeInMinutes() function";
 }
 
+/**
+ * Calculates the total working time. The time to preapre the given
+ * number of layers and the time that the lasagna has been in
+ * the oven.
+ * 
+ * @param layers 
+ * @param minutesInOven 
+ * @return Int
+ */
 function totalTimeInMinutes(layers:Int, minutesInOven:Int):Int {
   throw "Please implement the totalMinutesInOven() function";
 }
