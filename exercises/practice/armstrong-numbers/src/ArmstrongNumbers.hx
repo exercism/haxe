7e095f0844a20f19447324dd3f4e18f532b4829a
András B Nagy <20251272+BNAndras@users.noreply.github.com>
2023-08-23 07:45:58 -0700

    Update ArmstrongNumbers.hx

--- exercises/practice/armstrong-numbers/src/ArmstrongNumbers.hx
+++ exercises/practice/armstrong-numbers/src/ArmstrongNumbers.hx
@@ -2,14 +2,6 @@ package;
 
 class ArmstrongNumbers {
     public static function isArmstrongNumber(number: Int): Bool {
-        var numString = Std.string(number);
-        var digitCount = numString.length;
-
-        var total = 0.0;
-        for (char in numString.split("")) {
-            total += Math.pow(Std.parseInt(char), digitCount);
-        }
-
-        return total == number;
+        throw "Not Implemented"; // Delete this statement and write your own implementation.
     }
 }
