256bdcf6ac3d26749f9fbe8bdddc9ea79c5f99f1
András B Nagy <20251272+BNAndras@users.noreply.github.com>
2023-08-23 09:28:37 -0700

    Update ResistorColorDuo.hx

--- exercises/practice/resistor-color-duo/src/ResistorColorDuo.hx
+++ exercises/practice/resistor-color-duo/src/ResistorColorDuo.hx
@@ -2,6 +2,6 @@ package;
 
 class ResistorColorDuo {
 	public static function value(colors: Array<String>): Int {
-        throw "Not Implemented"; // Delete this statement and write your own implementation.
+		throw "Not Implemented"; // Delete this statement and write your own implementation.
 	}
 }
