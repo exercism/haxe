363f6930ca95fb18c59b81d7e0be04136f5f0091
András B Nagy <20251272+BNAndras@users.noreply.github.com>
2023-08-23 07:44:08 -0700

    Sync exercise to v3 specs

--- exercises/armstrong-numbers/src/ArmstrongNumbers.hx
+++
@@ -1,7 +0,0 @@
-package;
-
-class ArmstrongNumbers {
-    public static function isArmstrongNumber(number: Int): Bool {
-        throw "Not Implemented"; // Delete this statement and write your own implementation.
-    }
-}
