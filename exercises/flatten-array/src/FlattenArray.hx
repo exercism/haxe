c098b53132a62e94799d65358f4869ed1c46dc8c
András B Nagy <20251272+BNAndras@users.noreply.github.com>
2023-08-22 21:55:31 -0700

    Sync exercise to v3 specs and add missing tests

--- exercises/flatten-array/src/FlattenArray.hx
+++
@@ -1,7 +0,0 @@
-package;
-
-class FlattenArray {
-    public static function flatten(array: Array<Dynamic>): Array<Dynamic> {
-        throw "Not Implemented"; // Delete this statement and write your own implementation.
-    }
-}
