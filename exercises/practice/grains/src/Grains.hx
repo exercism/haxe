d719ba3b280b3f630fbb43d093df6124537ad04c
András B Nagy <20251272+BNAndras@users.noreply.github.com>
2023-08-23 07:59:42 -0700

    Standardize indent

--- exercises/practice/grains/src/Grains.hx
+++ exercises/practice/grains/src/Grains.hx
@@ -1,11 +1,10 @@
 package;
 
 class Grains {
-    public static function square(square: Int): Float {
-        throw "Not Implemented"; // Delete this statement and write your own implementation.
+	public static function square(square: Int): Float {
+		throw "Not Implemented"; // Delete this statement and write your own implementation.
     }
-
 	public static function total(): Float {
-        throw "Not Implemented"; // Delete this statement and write your own implementation.
+		throw "Not Implemented"; // Delete this statement and write your own implementation.
     } 
 }
