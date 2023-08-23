1b73cfcb18d041c1033a1bb110bf50e3670b35d3
András B Nagy <20251272+BNAndras@users.noreply.github.com>
2023-08-22 22:20:00 -0700

    Sync exercise to v3 and add missing test

--- exercises/practice/pangram/test/Test.hx
+++ exercises/practice/pangram/test/Test.hx
@@ -9,41 +9,32 @@ class Test extends buddy.SingleSuite {
 			it("empty sentence", {
 				Pangram.isPangram("").should.be(false);
 			});
-			it("perfect lower case", {
-				pending("Skipping");
+			xit("perfect lower case", {
 				Pangram.isPangram("abcdefghijklmnopqrstuvwxyz").should.be(true);
 			});
-			it("only lower case", {
-				pending("Skipping");
+			xit("only lower case", {
 				Pangram.isPangram("the quick brown fox jumps over the lazy dog").should.be(true);
 			});
-			it("missing the letter 'x'", {
-				pending("Skipping");
+			xit("missing the letter 'x'", {
 				Pangram.isPangram("a quick movement of the enemy will jeopardize five gunboats").should.be(false);
 			});
-			it("missing the letter 'h'", {
-				pending("Skipping");
+			xit("missing the letter 'h'", {
 				Pangram.isPangram("five boxing wizards jump quickly at it").should.be(false);
 			});
-			it("with underscores", {
-				pending("Skipping");
+			xit("with underscores", {
 				Pangram.isPangram("the_quick_brown_fox_jumps_over_the_lazy_dog").should.be(true);
 			});
-			it("with numbers", {
-				pending("Skipping");
+			xit("with numbers", {
 				Pangram.isPangram("the 1 quick brown fox jumps over the 2 lazy dogs").should.be(true);
 			});
-			it("missing letters replaced by numbers", {
-				pending("Skipping");
+			xit("missing letters replaced by numbers", {
 				Pangram.isPangram("7h3 qu1ck brown fox jumps ov3r 7h3 lazy dog").should.be(false);
 			});
-			it("mixed case and punctuation", {
-				pending("Skipping");
-				Pangram.isPangram("Five quacking Zephyrs jolt my wax bed.").should.be(true);
+			xit("mixed case and punctuation", {
+				Pangram.isPangram("\"Five quacking Zephyrs jolt my wax bed.\"").should.be(true);
 			});
-			it("case insensitive", {
-				// pending("Skipping");
-				Pangram.isPangram("the quick brown fox jumps over with lazy FX").should.be(false);
+			xit("a-m and A-M are 26 different characters but not a pangram", {
+				Pangram.isPangram("abcdefghijklm ABCDEFGHIJKLM").should.be(false);
 			});
 		});
 	}
