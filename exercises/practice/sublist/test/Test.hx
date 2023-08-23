222a53d34b1df54269ec0e843bc0e1a80d98d95e
András B Nagy <20251272+BNAndras@users.noreply.github.com>
2023-08-22 21:29:59 -0700

    Sync exercise to v3 specs and add missing test

--- exercises/practice/sublist/test/Test.hx
+++ exercises/practice/sublist/test/Test.hx
@@ -9,70 +9,55 @@ class Test extends buddy.SingleSuite {
 			it("empty lists", {
 				Sublist.sublist([], []).should.be("equal");
 			});
-			it("empty list within non empty list", {
-				pending("Skipping");
+			xit("empty list within non empty list", {
 				Sublist.sublist([], [1, 2, 3]).should.be("sublist");
-
 			});
-			it("non empty list contains empty list", {
-				pending("Skipping");
+			xit("non empty list contains empty list", {
 				Sublist.sublist([1, 2, 3], []).should.be("superlist");
-
 			});
-			it("list equals itself", {
-				pending("Skipping");
+			xit("list equals itself", {
 				Sublist.sublist([1, 2, 3], [1, 2, 3]).should.be("equal");
 			});
-			it("different lists", {
-				pending("Skipping");
+			xit("different lists", {
 				Sublist.sublist([1, 2, 3], [2, 3, 4]).should.be("unequal");
 			});
-			it("false start", {
-				pending("Skipping");
+			xit("false start", {
 				Sublist.sublist([1, 2, 5], [0, 1, 2, 3, 1, 2, 5, 6]).should.be("sublist");
 			});
-			it("consecutive", {
-				pending("Skipping");
+			xit("consecutive", {
 				Sublist.sublist([1, 1, 2], [0, 1, 1, 1, 2, 1, 2]).should.be("sublist");
 			});
-			it("sublist at start", {
-				pending("Skipping");
+			xit("sublist at start", {
 				Sublist.sublist([0, 1, 2], [0, 1, 2, 3, 4, 5]).should.be("sublist");
 			});
-			it("sublist in middle", {
-				pending("Skipping");
+			xit("sublist in middle", {
 				Sublist.sublist([2, 3, 4], [0, 1, 2, 3, 4, 5]).should.be("sublist");
 			});
-			it("sublist at end", {
-				pending("Skipping");
+			xit("sublist at end", {
 				Sublist.sublist([3, 4, 5], [0, 1, 2, 3, 4, 5]).should.be("sublist");
 			});
-			it("at start of superlist", {
-				pending("Skipping");
+			xit("at start of superlist", {
 				Sublist.sublist([0, 1, 2, 3, 4, 5], [0, 1, 2]).should.be("superlist");
 			});
-			it("in middle of superlist", {
-				pending("Skipping");
+			xit("in middle of superlist", {
 				Sublist.sublist([0, 1, 2, 3, 4, 5], [2, 3]).should.be("superlist");
 			});
-			it("at end of superlist", {
-				pending("Skipping");
+			xit("at end of superlist", {
 				Sublist.sublist([0, 1, 2, 3, 4, 5], [3, 4, 5]).should.be("superlist");
 			});
-			it("first list missing element from second list", {
-				pending("Skipping");
+			xit("first list missing element from second list", {
 				Sublist.sublist([1, 3], [1, 2, 3]).should.be("unequal");
 			});
-			it("second list missing element from first list", {
-				pending("Skipping");
+			xit("second list missing element from first list", {
 				Sublist.sublist([1, 2, 3], [1, 3]).should.be("unequal");
 			});
-			it("order matters to a list", {
-				pending("Skipping");
+			xit("first list missing additional digits from second list", {
+				Sublist.sublist([1, 2], [1, 22]).should.be("unequal");
+			});
+			xit("order matters to a list", {
 				Sublist.sublist([1, 2, 3], [3, 2, 1]).should.be("unequal");
 			});
-			it("same digits but different numbers", {
-				pending("Skipping");
+			xit("same digits but different numbers", {
 				Sublist.sublist([1, 0, 1], [10, 1]).should.be("unequal");
 			});
 		});
