65bd44699a187fbece4c3cf7eb51fea40c1dde56
András B Nagy <20251272+BNAndras@users.noreply.github.com>
2023-08-22 21:56:33 -0700

    Revert stub solution file

--- exercises/practice/flatten-array/src/FlattenArray.hx
+++ exercises/practice/flatten-array/src/FlattenArray.hx
@@ -2,22 +2,6 @@ package;
 
 class FlattenArray {
     public static function flatten(array: Array<Dynamic>): Array<Dynamic> {
-        var accumulator = [];
-        for (item in array) {
-            if (isArray(item))
-                accumulator = accumulator.concat(flatten(item));
-            else if (item == null)
-                continue;
-            else
-                accumulator.push(item);
-        }
-
-        return accumulator;
-    } 
-
-    private static inline function isArray(obj: Dynamic)
-        return switch(Type.typeof(obj)) {
-            case TClass(Array): true;
-            default:            false; 
-        }
+        throw "Not Implemented"; // Delete this statement and write your own implementation.
+    }
 }
