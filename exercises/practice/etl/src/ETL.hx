22d656648a42ee5c76dd96e0ead684d138a34965
Noah Betzen <noah@nezteb.net>
2023-08-31 04:01:36 +0000

    Change etl exercise to PascalCase

--- exercises/practice/etl/src/ETL.hx
+++
@@ -1,8 +0,0 @@
-typedef LegacyData = Map<String, Array<String>>;
-typedef NewData    = Map<String, Int>;
-
-class ETL {
-	public static function transform(input: LegacyData): NewData {
-		throw "Not Implemented"; // Delete this statement and write your own implementation.
-	}
-}
--- exercises/practice/etl/src/Etl.hx
+++ exercises/practice/etl/src/Etl.hx
@@ -2,9 +2,5 @@ typedef LegacyData = Map<String, Array<String>>;
 typedef NewData    = Map<String, Int>;
 
 function transform(input: LegacyData): NewData {
-	return [
-		for (k => v in input)
-			for (i in v)
-				i.toLowerCase() => Std.parseInt(k)
-	];
+	throw "Not Implemented"; // Delete this statement and write your own implementation.
 }
