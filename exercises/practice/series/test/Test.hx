b8aed3137873f44f4e8ac60715402b9c6bd7aa13
András B Nagy <20251272+BNAndras@users.noreply.github.com>
2023-09-05 11:00:40 -0700

    Update exercise to v3 specs

--- exercises/practice/series/test/Test.hx
+++ exercises/practice/series/test/Test.hx
@@ -9,40 +9,40 @@ class Test extends buddy.SingleSuite {
 			it("slices of one from one", {
 				Series.slices(1, "1").should.containExactly(["1"]);
 			});
-			it("slices of one from two", {
-				pending("Skipping");
+
+			xit("slices of one from two", {
 				Series.slices(1, "12").should.containExactly(["1", "2"]);
 			});
-			it("slices of two", {
-				pending("Skipping");
+
+			xit("slices of two", {
 				Series.slices(2, "35").should.containExactly(["35"]);
 			});
-			it("slices of two overlap", {
-				pending("Skipping");
+
+			xit("slices of two overlap", {
 				Series.slices(2, "9142").should.containExactly(["91", "14", "42"]);
 			});
-			it("slices can include duplicates", {
-				pending("Skipping");
+
+			xit("slices can include duplicates", {
 				Series.slices(3, "777777").should.containExactly(["777", "777", "777", "777"]);
 			});
-			it("slices of a long series", {
-				pending("Skipping");
+
+			xit("slices of a long series", {
 				Series.slices(5, "918493904243").should.containExactly(["91849", "18493", "84939", "49390", "93904", "39042", "90424", "04243"]);
 			});
-			it("slice length is too large", {
-				pending("Skipping");
+
+			xit("slice length is too large", {
 				Series.slices.bind(6, "12345").should.throwValue("slice length cannot be greater than series length");
 			});
-			it("slice length cannot be zero", {
-				pending("Skipping");
+
+			xit("slice length cannot be zero", {
 				Series.slices.bind(0, "12345").should.throwValue("slice length cannot be zero");
 			});
-			it("slice length cannot be negative", {
-				pending("Skipping");
+
+			xit("slice length cannot be negative", {
 				Series.slices.bind(-1, "123").should.throwValue("slice length cannot be negative");
 			});
-			it("empty series is invalid", {
-				pending("Skipping");
+
+			xit("empty series is invalid", {
 				Series.slices.bind(1, "").should.throwValue("series cannot be empty");
 			});
 		});
--- exercises/series/src/Series.hx
+++
@@ -1,7 +0,0 @@
-package;
-
-class Series {
-    public static function slices(sliceLength: Int, series: String): Array<String> {
-        throw "Not Implemented"; // Delete this statement and write your own implementation.
-    } 
-}
