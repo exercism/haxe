df8ae6a1e7cda96903d4cc69358a490b4ba5ace6
András B Nagy <20251272+BNAndras@users.noreply.github.com>
2023-09-06 16:55:09 -0700

    Update files to v3 specs

--- exercises/practice/clock/test/Test.hx
+++ exercises/practice/clock/test/Test.hx
@@ -9,151 +9,150 @@ class Test extends buddy.SingleSuite {
 			it("on the hour", {
 				Clock.create(8, 0).should.be("08:00");
 			});
-			it("past the hour", {
-				pending("Skipping");
+
+			xit("past the hour", {
 				Clock.create(11, 9).should.be("11:09");
 			});
-			it("midnight is zero hours", {
-				pending("Skipping");
+
+			xit("midnight is zero hours", {
 				Clock.create(24, 0).should.be("00:00");
 			});
-			it("hour rolls over", {
-				pending("Skipping");
+
+			xit("hour rolls over", {
 				Clock.create(25, 0).should.be("01:00");
 			});
-			it("hour rolls over continuously", {
-				pending("Skipping");
+
+			xit("hour rolls over continuously", {
 				Clock.create(100, 0).should.be("04:00");
 			});
-			it("sixty minutes is next hour", {
-				pending("Skipping");
+
+			xit("sixty minutes is next hour", {
 				Clock.create(1, 60).should.be("02:00");
 			});
-			it("minutes roll over", {
-				pending("Skipping");
+
+			xit("minutes roll over", {
 				Clock.create(0, 160).should.be("02:40");
 			});
-			it("minutes roll over continuously", {
-				pending("Skipping");
+
+			xit("minutes roll over continuously", {
 				Clock.create(0, 1723).should.be("04:43");
 			});
-			it("hour and minutes roll over", {
-				pending("Skipping");
+
+			xit("hour and minutes roll over", {
 				Clock.create(25, 160).should.be("03:40");
 			});
-			it("hour and minutes roll over continuously", {
-				pending("Skipping");
+
+			xit("hour and minutes roll over continuously", {
 				Clock.create(201, 3001).should.be("11:01");
 			});
-			it("hour and minutes roll over to exactly midnight", {
-				pending("Skipping");
+
+			xit("hour and minutes roll over to exactly midnight", {
 				Clock.create(72, 8640).should.be("00:00");
 			});
-			it("negative hour", {
-				pending("Skipping");
+
+			xit("negative hour", {
 				Clock.create(-1, 15).should.be("23:15");
 			});
-			it("negative hour rolls over", {
-				pending("Skipping");
+
+			xit("negative hour rolls over", {
 				Clock.create(-25, 0).should.be("23:00");
 			});
-			it("negative hour rolls over continuously", {
-				pending("Skipping");
+
+			xit("negative hour rolls over continuously", {
 				Clock.create(-91, 0).should.be("05:00");
 			});
-			it("negative minutes", {
-				pending("Skipping");
+
+			xit("negative minutes", {
 				Clock.create(1, -40).should.be("00:20");
 			});
-			it("negative minutes roll over", {
-				pending("Skipping");
+
+			xit("negative minutes roll over", {
 				Clock.create(1, -160).should.be("22:20");
 			});
-			it("negative minutes roll over continuously", {
-				pending("Skipping");
+
+			xit("negative minutes roll over continuously", {
 				Clock.create(1, -4820).should.be("16:40");
 			});
-			it("negative sixty minutes is previous hour", {
-				pending("Skipping");
+
+			xit("negative sixty minutes is previous hour", {
 				Clock.create(2, -60).should.be("01:00");
 			});
-			it("negative hour and minutes both roll over", {
-				pending("Skipping");
+
+			xit("negative hour and minutes both roll over", {
 				Clock.create(-25, -160).should.be("20:20");
 			});
-			it("negative hour and minutes both roll over continuously", {
-				pending("Skipping");
+
+			xit("negative hour and minutes both roll over continuously", {
 				Clock.create(-121, -5810).should.be("22:10");
 			});
 		});
 		describe("Add minutes", {
-			it("add minutes", {
+			xit("add minutes", {
 				Clock.add(10, 0, 3).should.be("10:03");
 			});
-			it("add no minutes", {
-				pending("Skipping");
+
+			xit("add no minutes", {
 				Clock.add(6, 41, 0).should.be("06:41");
 			});
-			it("add to next hour", {
-				pending("Skipping");
+
+			xit("add to next hour", {
 				Clock.add(0, 45, 40).should.be("01:25");
 			});
-			it("add more than one hour", {
-				pending("Skipping");
+
+			xit("add more than one hour", {
 				Clock.add(10, 0, 61).should.be("11:01");
 			});
-			it("add more than two hours with carry", {
-				pending("Skipping");
+
+			xit("add more than two hours with carry", {
 				Clock.add(0, 45, 160).should.be("03:25");
 			});
-			it("add across midnight", {
-				pending("Skipping");
+
+			xit("add across midnight", {
 				Clock.add(23, 59, 2).should.be("00:01");
 			});
-			it("add more than one day (1500 min = 25 hrs)", {
-				pending("Skipping");
+
+			xit("add more than one day (1500 min = 25 hrs)", {
 				Clock.add(5, 32, 1500).should.be("06:32");
 			});
-			it("add more than two days", {
-				pending("Skipping");
+
+			xit("add more than two days", {
 				Clock.add(1, 1, 3500).should.be("11:21");
 			});
 		});
 		describe("Subtract minutes", {
-			it("subtract minutes", {
+			xit("subtract minutes", {
 				Clock.subtract(10, 3, 3).should.be("10:00");
 			});
-			it("subtract to previous hour", {
-				pending("Skipping");
+
+			xit("subtract to previous hour", {
 				Clock.subtract(10, 3, 30).should.be("09:33");
 			});
-			it("subtract more than an hour", {
-				pending("Skipping");
+
+			xit("subtract more than an hour", {
 				Clock.subtract(10, 3, 70).should.be("08:53");
 			});
-			it("subtract across midnight", {
-				pending("Skipping");
+
+			xit("subtract across midnight", {
 				Clock.subtract(0, 3, 4).should.be("23:59");
 			});
-			it("subtract more than two hours", {
-				pending("Skipping");
+			xit("subtract more than two hours", {
 				Clock.subtract(0, 0, 160).should.be("21:20");
 			});
-			it("subtract more than two hours with borrow", {
-				pending("Skipping");
+
+			xit("subtract more than two hours with borrow", {
 				Clock.subtract(6, 15, 160).should.be("03:35");
 			});
-			it("subtract more than one day (1500 min = 25 hrs)", {
-				pending("Skipping");
+
+			xit("subtract more than one day (1500 min = 25 hrs)", {
 				Clock.subtract(5, 32, 1500).should.be("04:32");
 			});
-			it("subtract more than two days", {
-				pending("Skipping");
+
+			xit("subtract more than two days", {
 				Clock.subtract(2, 20, 3000).should.be("00:20");
 			});
 		});
 		describe("Compare two clocks for equality", {
-			it("clocks with same time", {
+			xit("clocks with same time", {
 				Clock.equal({
 					hour: 15,
 					minute: 37
@@ -162,8 +161,8 @@ class Test extends buddy.SingleSuite {
 					minute: 37
 				}).should.be(true);
 			});
-			it("clocks a minute apart", {
-				pending("Skipping");
+
+			xit("clocks a minute apart", {
 				Clock.equal({
 					hour: 15,
 					minute: 36
@@ -172,8 +171,8 @@ class Test extends buddy.SingleSuite {
 					minute: 37
 				}).should.be(false);
 			});
-			it("clocks an hour apart", {
-				pending("Skipping");
+
+			xit("clocks an hour apart", {
 				Clock.equal({
 					hour: 14,
 					minute: 37
@@ -182,8 +181,8 @@ class Test extends buddy.SingleSuite {
 					minute: 37
 				}).should.be(false);
 			});
-			it("clocks with hour overflow", {
-				pending("Skipping");
+
+			xit("clocks with hour overflow", {
 				Clock.equal({
 					hour: 10,
 					minute: 37
@@ -192,8 +191,8 @@ class Test extends buddy.SingleSuite {
 					minute: 37
 				}).should.be(true);
 			});
-			it("clocks with hour overflow by several days", {
-				pending("Skipping");
+
+			xit("clocks with hour overflow by several days", {
 				Clock.equal({
 					hour: 3,
 					minute: 11
@@ -202,8 +201,8 @@ class Test extends buddy.SingleSuite {
 					minute: 11
 				}).should.be(true);
 			});
-			it("clocks with negative hour", {
-				pending("Skipping");
+
+			xit("clocks with negative hour", {
 				Clock.equal({
 					hour: 22,
 					minute: 40
@@ -212,8 +211,8 @@ class Test extends buddy.SingleSuite {
 					minute: 40
 				}).should.be(true);
 			});
-			it("clocks with negative hour that wraps", {
-				pending("Skipping");
+
+			xit("clocks with negative hour that wraps", {
 				Clock.equal({
 					hour: 17,
 					minute: 3
@@ -222,8 +221,8 @@ class Test extends buddy.SingleSuite {
 					minute: 3
 				}).should.be(true);
 			});
-			it("clocks with negative hour that wraps multiple times", {
-				pending("Skipping");
+
+			xit("clocks with negative hour that wraps multiple times", {
 				Clock.equal({
 					hour: 13,
 					minute: 49
@@ -232,8 +231,8 @@ class Test extends buddy.SingleSuite {
 					minute: 49
 				}).should.be(true);
 			});
-			it("clocks with minute overflow", {
-				pending("Skipping");
+
+			xit("clocks with minute overflow", {
 				Clock.equal({
 					hour: 0,
 					minute: 1
@@ -242,8 +241,8 @@ class Test extends buddy.SingleSuite {
 					minute: 1441
 				}).should.be(true);
 			});
-			it("clocks with minute overflow by several days", {
-				pending("Skipping");
+
+			xit("clocks with minute overflow by several days", {
 				Clock.equal({
 					hour: 2,
 					minute: 2
@@ -252,8 +251,8 @@ class Test extends buddy.SingleSuite {
 					minute: 4322
 				}).should.be(true);
 			});
-			it("clocks with negative minute", {
-				pending("Skipping");
+
+			xit("clocks with negative minute", {
 				Clock.equal({
 					hour: 2,
 					minute: 40
@@ -262,8 +261,8 @@ class Test extends buddy.SingleSuite {
 					minute: -20
 				}).should.be(true);
 			});
-			it("clocks with negative minute that wraps", {
-				pending("Skipping");
+
+			xit("clocks with negative minute that wraps", {
 				Clock.equal({
 					hour: 4,
 					minute: 10
@@ -272,8 +271,8 @@ class Test extends buddy.SingleSuite {
 					minute: -1490
 				}).should.be(true);
 			});
-			it("clocks with negative minute that wraps multiple times", {
-				pending("Skipping");
+
+			xit("clocks with negative minute that wraps multiple times", {
 				Clock.equal({
 					hour: 6,
 					minute: 15
@@ -282,8 +281,8 @@ class Test extends buddy.SingleSuite {
 					minute: -4305
 				}).should.be(true);
 			});
-			it("clocks with negative hours and minutes", {
-				pending("Skipping");
+
+			xit("clocks with negative hours and minutes", {
 				Clock.equal({
 					hour: 7,
 					minute: 32
@@ -292,8 +291,8 @@ class Test extends buddy.SingleSuite {
 					minute: -268
 				}).should.be(true);
 			});
-			it("clocks with negative hours and minutes that wrap", {
-				pending("Skipping");
+
+			xit("clocks with negative hours and minutes that wrap", {
 				Clock.equal({
 					hour: 18,
 					minute: 7
@@ -302,8 +301,8 @@ class Test extends buddy.SingleSuite {
 					minute: -11513
 				}).should.be(true);
 			});
-			it("full clock and zeroed clock", {
-				pending("Skipping");
+
+			xit("full clock and zeroed clock", {
 				Clock.equal({
 					hour: 24,
 					minute: 0
