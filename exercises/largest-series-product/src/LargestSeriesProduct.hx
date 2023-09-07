c7065790dcc6eb3db90d11e5dead83e6a3d2f5c9
András B Nagy <20251272+BNAndras@users.noreply.github.com>
2023-09-06 21:49:27 -0700

    Sync files to v3 specs and add new test

--- exercises/largest-series-product/src/LargestSeriesProduct.hx
+++
@@ -1,7 +0,0 @@
-package;
-
-class LargestSeriesProduct {
-    public static function largestProduct(span: Int, digits: String): Int {
-        throw "Not Implemented"; // Delete this statement and write your own implementation.
-    } 
-}
--- exercises/practice/largest-series-product/test/Test.hx
+++ exercises/practice/largest-series-product/test/Test.hx
@@ -9,61 +9,63 @@ class Test extends buddy.SingleSuite {
 			it("finds the largest product if span equals length", {
 				LargestSeriesProduct.largestProduct(2, "29").should.be(18);
 			});
-			it("can find the largest product of 2 with numbers in order", {
-				pending("Skipping");
+
+			xit("can find the largest product of 2 with numbers in order", {
 				LargestSeriesProduct.largestProduct(2, "0123456789").should.be(72);
 			});
-			it("can find the largest product of 2", {
-				pending("Skipping");
+
+			xit("can find the largest product of 2", {
 				LargestSeriesProduct.largestProduct(2, "576802143").should.be(48);
 			});
-			it("can find the largest product of 3 with numbers in order", {
-				pending("Skipping");
+
+			xit("can find the largest product of 3 with numbers in order", {
 				LargestSeriesProduct.largestProduct(3, "0123456789").should.be(504);
 			});
-			it("can find the largest product of 3", {
-				pending("Skipping");
+
+			xit("can find the largest product of 3", {
 				LargestSeriesProduct.largestProduct(3, "1027839564").should.be(270);
 			});
-			it("can find the largest product of 5 with numbers in order", {
-				pending("Skipping");
+
+			xit("can find the largest product of 5 with numbers in order", {
 				LargestSeriesProduct.largestProduct(5, "0123456789").should.be(15120);
 			});
-			it("can get the largest product of a big number", {
-				pending("Skipping");
-				LargestSeriesProduct.largestProduct(6, "73167176531330624919225119674426574742355349194934").should.be(23520);
+
+			xit("can get the largest product of a big number", {
+				var digits = "73167176531330624919225119674426574742355349194934";
+
+				LargestSeriesProduct.largestProduct(6, digits).should.be(23520);
 			});
-			it("reports zero if the only digits are zero", {
-				pending("Skipping");
+
+			xit("reports zero if the only digits are zero", {
 				LargestSeriesProduct.largestProduct(2, "0000").should.be(0);
 			});
-			it("reports zero if all spans include zero", {
-				pending("Skipping");
+
+			xit("reports zero if all spans include zero", {
 				LargestSeriesProduct.largestProduct(3, "99099").should.be(0);
 			});
-			it("rejects span longer than string length", {
-				pending("Skipping");
+
+			xit("rejects span longer than string length", {
 				LargestSeriesProduct.largestProduct.bind(4, "123").should.throwValue("span must be smaller than string length");
 			});
-			it("reports 1 for empty string and empty product (0 span)", {
-				pending("Skipping");
+
+			xit("reports 1 for empty string and empty product (0 span)", {
 				LargestSeriesProduct.largestProduct(0, "").should.be(1);
 			});
-			it("reports 1 for nonempty string and empty product (0 span)", {
-				pending("Skipping");
+
+			xit("reports 1 for nonempty string and empty product (0 span)", {
 				LargestSeriesProduct.largestProduct(0, "123").should.be(1);
 			});
-			it("rejects empty string and nonzero span", {
-				pending("Skipping");
+
+			xit("rejects empty string and nonzero span", {
 				LargestSeriesProduct.largestProduct.bind(1, "").should.throwValue("span must be smaller than string length");
 			});
-			it("rejects invalid character in digits", {
-				pending("Skipping");
+
+			xit("rejects invalid character in digits", {
 				LargestSeriesProduct.largestProduct.bind(2, "1234a5").should.throwValue("digits input must only contain digits");
 			});
-			it("rejects negative span", {
-				pending("Skipping");
-				LargestSeriesProduct.largestProduct.bind(-1, "12345").should.throwValue("span must be greater than zero");
+
+			xit("rejects negative span", {
+				LargestSeriesProduct.largestProduct.bind(-1, "12345").should.throwValue("span must not be negative");
 			});
 		});
 	}
