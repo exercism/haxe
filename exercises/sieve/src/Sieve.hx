2f033de35bb11c89737576ce299514f0a752ffe5
András B Nagy <20251272+BNAndras@users.noreply.github.com>
2023-09-06 21:11:17 -0700

    Sync exercise to v3 specs

--- exercises/sieve/src/Sieve.hx
+++
@@ -1,7 +0,0 @@
-package;
-
-class Sieve {
-    public static function primes(limit: Int): Array<Int> {
-        throw "Not Implemented"; // Delete this statement and write your own implementation.
-    } 
-}
