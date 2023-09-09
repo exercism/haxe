cf4dc7d8498e60a1db6f3bfc80e5ce1637bcec1a
András B Nagy <20251272+BNAndras@users.noreply.github.com>
2023-09-08 17:39:42 -0700

    Update exercise files to v3 specs

--- exercises/all-your-base/src/AllYourBase.hx
+++
@@ -1,7 +0,0 @@
-package;
-
-class AllYourBase {
-    public static function rebase(inputBase: Int, outputBase: Int, digits: Array<Int>): Array<Int> {
-        throw "Not Implemented"; // Delete this statement and write your own implementation.
-    } 
-}
