a29696e551377b6e472227c34d3cd20666db36cd
András B Nagy <20251272+BNAndras@users.noreply.github.com>
2023-09-08 22:12:08 -0700

    Sync exercise files to v3 specs and add missing tests

--- exercises/prime-factors/src/PrimeFactors.hx
+++
@@ -1,7 +0,0 @@
-package;
-
-class PrimeFactors {
-    public static function factors(value: Float): Array<Float> {
-        throw "Not Implemented"; // Delete this statement and write your own implementation.
-    } 
-}
