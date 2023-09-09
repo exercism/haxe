3f65b4d06668daee96acf66f14fd3c85ab269904
András B Nagy <20251272+BNAndras@users.noreply.github.com>
2023-09-08 23:04:33 -0700

    Update exercise files to v3 specs and add missing tests

---
+++ exercises/practice/strain/src/Strain.hx
@@ -0,0 +1,9 @@
+class ArrayExt {
+	public static function keep<T>(array:Array<T>, fun:T->Bool) {
+		throw "Not Implemented"; // Delete this statement and write your own implementation.
+	}
+
+	public static function discard<T>(array:Array<T>, fun:T->Bool) {
+		throw "Not Implemented"; // Delete this statement and write your own implementation.
+	}
+}
