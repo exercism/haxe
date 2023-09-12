515cc6c6f3eb078883149f836bb38f6fd7ece528
András B Nagy <20251272+BNAndras@users.noreply.github.com>
2023-09-11 18:26:41 -0700

    Sync exercise files to v3

--- exercises/practice/minesweeper/test/Test.hx
+++ exercises/practice/minesweeper/test/Test.hx
@@ -9,36 +9,47 @@ class Test extends buddy.SingleSuite {
 			it("no rows", {
 				Minesweeper.annotate([]).should.containExactly([]);
 			});
+
 			xit("no columns", {
 				Minesweeper.annotate([""]).should.containExactly([""]);
 			});
+
 			xit("no mines", {
 				Minesweeper.annotate(["   ", "   ", "   "]).should.containExactly(["   ", "   ", "   "]);
 			});
+
 			xit("minefield with only mines", {
 				Minesweeper.annotate(["***", "***", "***"]).should.containExactly(["***", "***", "***"]);
 			});
+
 			xit("mine surrounded by spaces", {
 				Minesweeper.annotate(["   ", " * ", "   "]).should.containExactly(["111", "1*1", "111"]);
 			});
+
 			xit("space surrounded by mines", {
 				Minesweeper.annotate(["***", "* *", "***"]).should.containExactly(["***", "*8*", "***"]);
 			});
+			
 			xit("horizontal line", {
 				Minesweeper.annotate([" * * "]).should.containExactly(["1*2*1"]);
 			});
+
 			xit("horizontal line, mines at edges", {
 				Minesweeper.annotate(["*   *"]).should.containExactly(["*1 1*"]);
 			});
+
 			xit("vertical line", {
 				Minesweeper.annotate([" ", "*", " ", "*", " "]).should.containExactly(["1", "*", "2", "*", "1"]);
 			});
+
 			xit("vertical line, mines at edges", {
 				Minesweeper.annotate(["*", " ", " ", " ", "*"]).should.containExactly(["*", "1", " ", "1", "*"]);
 			});
+
 			xit("cross", {
 				Minesweeper.annotate(["  *  ", "  *  ", "*****", "  *  ", "  *  "]).should.containExactly([" 2*2 ", "25*52", "*****", "25*52", " 2*2 "]);
 			});
+
 			xit("large minefield", {
 				Minesweeper.annotate([" *  * ", "  *   ", "    * ", "   * *", " *  * ", "      "])
 					.should.containExactly(["1*22*1", "12*322", " 123*2", "112*4*", "1*22*2", "111111"]);
