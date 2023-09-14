604700c59f0f426e534d40dcc7beac337bddf6fd
András B Nagy <20251272+BNAndras@users.noreply.github.com>
2023-09-14 08:51:44 -0700

    Run haxelib formatter on files

--- exercises/practice/robot-simulator/src/RobotSimulator.hx
+++ exercises/practice/robot-simulator/src/RobotSimulator.hx
@@ -1,13 +1,13 @@
 class Robot {
-    public function new(x:Int, y:Int, direction:String) {
-        throw "Not Implemented"; // Delete this statement and write your own implementation.
-    }
-
-	public function move(instructions:String):Robot {
-        throw "Not Implemented"; // Delete this statement and write your own implementation.
+	public function new(x:Int, y:Int, direction:String) {
+		throw "Not Implemented"; // Delete this statement and write your own implementation.
 	}
 
-    public function equals(other:Robot):Bool {
-        throw "Not Implemented"; // Delete this statement and write your own implementation.
+	public function move(instructions:String):Robot {
+		throw "Not Implemented"; // Delete this statement and write your own implementation.
+	}
+
+	public function equals(other:Robot):Bool {
+		throw "Not Implemented"; // Delete this statement and write your own implementation.
 	}
 }
