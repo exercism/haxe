56822a2b9e889b438f1b92b9556c258a6c0e2e3e
András B Nagy <20251272+BNAndras@users.noreply.github.com>
2024-02-02 17:04:14 -0800

    pop count to eliud's eggs

--- exercises/practice/eliuds-eggs/test/Test.hx
+++ exercises/practice/eliuds-eggs/test/Test.hx
@@ -4,21 +4,21 @@ using buddy.Should;
 
 class Test extends buddy.SingleSuite {
 	public function new() {
-		describe("Pop count tests", {
+		describe("Eliud's Eggs tests", {
 			it("no factors", {
-				PopCount.eggCount(0).should.be(0);
+				EliudsEggs.eggCount(0).should.be(0);
 			});
 
 			xit("one egg", {
-				PopCount.eggCount(16).should.be(1);
+				EliudsEggs.eggCount(16).should.be(1);
 			});
 
 			xit("four eggs", {
-				PopCount.eggCount(89).should.be(4);
+				EliudsEggs.eggCount(89).should.be(4);
 			});
 
 			xit("Thirteen eggs", {
-				PopCount.eggCount(2000000000).should.be(13);
+				EliudsEggs.eggCount(2000000000).should.be(13);
 			});
 		});
 	}
